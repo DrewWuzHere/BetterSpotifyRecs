d748e505e1db75be4c34c37a58298c584c764d36
rosekoehler <rosekoehler13@gmail.com>
2025-10-12 11:56:54 -0600

    Update expiriment.py

--- expiriment.py
+++ expiriment.py
@@ -50,8 +50,7 @@ def runExpiriment():
         # hand this point to kd tree to find neighbors/song recs
         my_df.drop(columns=['track_name'])
         kd = KDTree(my_df.to_numpy())
-        avg_fts = avg_fts.loc[0].values.tolist()
-        nearest_dist, nearest_ID, check_box_nonexistent = kd.search(np.array(avg_fts))
+        nearest_dist, nearest_ID, check_box_nonexistent = kd.search(avg_fts.)
         if check_box_nonexistent:
             print("welp")
         print(nearest_dist, "is how close our graph comes to our average song point")
